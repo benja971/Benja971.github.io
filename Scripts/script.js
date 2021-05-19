3f7ef8d46edf53f8291af2412c655d63970185f3
benja971 <46868460+benja971@users.noreply.github.com>
2021-05-19 17:43:42 -0400

    underline selected page

--- Scripts/script.js
+++ Scripts/script.js
@@ -6,8 +6,30 @@ function getScroll() {
 
 addEventListener("scroll", (e) => {
     if (getScroll() > 0) {
-        document.querySelector("#logomenu").classList.add("top");
+        document.querySelector("nav").classList.add("top");
     } else {
-        document.querySelector("#logomenu").classList.remove("top");
+        document.querySelector("nav").classList.remove("top");
+    }
+});
+
+addEventListener("load", (e) => {
+    let title = document.title;
+    if (title.startsWith("Home")) {
+        document.getElementById("menu-item-1").classList.add("selected");
+    }
+    if (title.startsWith("About")) {
+        document.getElementById("menu-item-2").classList.add("selected");
+    }
+    if (title.startsWith("My curriculum")) {
+        document.getElementById("menu-item-3").classList.add("selected");
+    }
+    if (title.startsWith("My portfolio")) {
+        document.getElementById("menu-item-4").classList.add("selected");
+    }
+    if (title.startsWith("Contact")) {
+        document.getElementById("menu-item-5").classList.add("selected");
+    }
+    if (title.startsWith("My horse")) {
+        document.getElementById("menu-item-6").classList.add("selected");
     }
 });
