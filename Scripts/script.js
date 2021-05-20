0697802d7f6a7ce80d5ea562ef29ae3cd450215a
benja971 <46868460+benja971@users.noreply.github.com>
2021-05-20 12:19:24 -0400

    ad gitignore

---
+++ Scripts/script.js
@@ -0,0 +1,18 @@
+function getScroll() {
+    var h = document.documentElement,
+        st = "scrollTop";
+    return h[st];
+}
+
+addEventListener("scroll", (e) => {
+    if (getScroll() > 0) {
+        document.body.classList.add("top");
+    } else {
+        document.body.classList.remove("top");
+    }
+});
+
+addEventListener("load", (e) => {
+    const id = document.title.toLowerCase().split(" - ")[0].replaceAll(" ", "-");
+    document.getElementById(id).classList.add("selected");
+});
